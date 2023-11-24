7f7ee6dacbc99261dea5395480e42a30da20918c
ianlandingin <iclandingin28@gmail.com>
2023-11-24 08:21:16 +0800

    server side: Converted requires to imports for ES6

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-require("dotenv").config();
+import express from "express";
+import dotenv from "dotenv";
 
-const express = require("express");
+const env = dotenv.config();
 
 const APP = express();
 
-port = process.env.PORT;
+const PORT = process.env.PORT;
 
-APP.listen(port, () => {
-  console.log(`Server is up and listening to port ${port}`);
+APP.listen(PORT, () => {
+  console.log(`Server is up and listening to port ${PORT}`);
 });
