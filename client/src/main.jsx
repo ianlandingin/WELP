561d0a92a7f78274134a5960e11f62865d79c423
ianlandingin <iclandingin28@gmail.com>
2023-11-28 12:21:49 +0800

    frontend: Wrapped App component in RestaurantContextProvider component for context management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { RestaurantContextProvider } from "./context/RestaurantsContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
+  <RestaurantContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </RestaurantContextProvider>
 );
