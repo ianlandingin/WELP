edf00eb01bcda218bcc6eb940130ff23bee74166
ianlandingin <iclandingin28@gmail.com>
2023-11-28 08:08:39 +0800

    Installed react-router-dom and integrated SPA to the App and main jsx files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import DetailsPage from "./routes/DetailsPage";
 
 function App() {
   return (
     <>
-      <div>App</div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/restaurants/:id" element={<DetailsPage />} />
+        <Route exact path="/restaurants/:id/update" element={<UpdatePage />} />
+      </Routes>
     </>
   );
 }
