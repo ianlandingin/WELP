52b434eccec9382a6ababac548cf9ad284ab7121
ianlandingin <iclandingin28@gmail.com>
2023-11-28 12:23:01 +0800

    frontend: integrated RestaurantContext into RestaurantList component via useComponent and integrated backend API

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,11 +1,28 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import RestaurantFinder from "../APIs/RestaurantsFinder";
+import { RestaurantContext } from "../context/RestaurantsContext";
 
-const RestaurantList = () => {
+// eslint-disable-next-line no-unused-vars
+const RestaurantList = (props) => {
+  const { restaurants, setRestaurants } = useContext(RestaurantContext);
+  useEffect(() => {
+    const fetchRestaurantData = async () => {
+      await RestaurantFinder.get("/")
+        .then((response) => {
+          console.log(response.data.restaurants);
+          setRestaurants(response.data.restaurants);
+        })
+        .catch((error) => console.log(error));
+    };
+
+    fetchRestaurantData();
+  }, [setRestaurants]);
+
   return (
     <div className="list-group">
-      <table className="table table-hover table-dark">
-        <thead>
+      <table className="table table-hover table-striped">
+        <thead className="thead-dark">
           <tr className="bg-primary">
             <th scope="col">Restaurant</th>
             <th scope="col">Location</th>
@@ -16,22 +33,45 @@ const RestaurantList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Shakeys</td>
-            <td>Malate</td>
-            <td>$$</td>
-            <td>Rating</td>
-            <td>
-              <button type="button" className="btn btn-warning">
-                Update
-              </button>
-            </td>
-            <td>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </td>
-          </tr>
+          {restaurants ? (
+            restaurants.map((restaurant) => {
+              return (
+                <tr key={restaurant?.id}>
+                  <td>{restaurant?.name}</td>
+                  <td>{restaurant?.location}</td>
+                  <td>{"$".repeat(restaurant?.price_range)}</td>
+                  <td>Rating</td>
+                  <td>
+                    <button type="button" className="btn btn-warning">
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button type="button" className="btn btn-danger">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td>Shakeys</td>
+              <td>Malate</td>
+              <td>$$</td>
+              <td>Rating</td>
+              <td>
+                <button type="button" className="btn btn-warning">
+                  Update
+                </button>
+              </td>
+              <td>
+                <button type="button" className="btn btn-danger">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
