bb25fe45eeb67439a5b8c75d8c9929c8d89d0fdf
ianlandingin <iclandingin28@gmail.com>
2023-12-05 09:30:24 +0800

    frontend: Feat: Implemented API integration for detailspage.jsx

--- client/src/routes/DetailsPage.jsx
+++ client/src/routes/DetailsPage.jsx
@@ -1,8 +1,39 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import RestaurantFinder from "../APIs/RestaurantsFinder";
 
 const DetailsPage = () => {
-  return <div>DetailsPage</div>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchRestaurantData = async () => {
+      await RestaurantFinder.get(`/${id}`)
+        .then((response) => {
+          setName(response?.data?.restaurant?.name);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+        });
+    };
+
+    fetchRestaurantData();
+  }, [id]);
+  return (
+    <div>
+      {loading ? (
+        <h1 className="text-center">Loading...</h1>
+      ) : (
+        <h1 className="text-center">{name}</h1>
+      )}
+    </div>
+  );
 };
 
 export default DetailsPage;
