f14cbd017171c7c0588bc1e9ed17efa230a87b89
ianlandingin <iclandingin28@gmail.com>
2024-01-05 14:24:30 +0800

    Frontend: Feat: Created AddReview form component for detailspage

---
+++ client/src/components/AddReview.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const AddReview = () => {
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState("Rating");
+  const [reviewText, setReviewText] = useState("");
+
+  return (
+    <div className="mb-2">
+      <form action="">
+        <div className="form-row">
+          <div className="form-group col-8">
+            <label htmlFor="name">Name</label>
+            <input
+              value={name}
+              id="name"
+              placeholder="Name"
+              type="text"
+              className="form-control"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-4">
+            <label htmlFor="rating">Rating</label>
+            <select
+              value={rating}
+              name="rating"
+              id="rating"
+              className="custom-select"
+              onChange={(e) => setRating(e.target.value)}
+            >
+              <option disabled>Rating</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="review">Review</label>
+          <textarea
+            value={reviewText}
+            name="review"
+            id="review"
+            className="form-control"
+            onChange={(e) => setReviewText(e.target.value)}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddReview;
